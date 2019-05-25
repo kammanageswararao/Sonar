710f1d3fee3ce0c46feacf5d5c4e573fee3132e2
kammanageswararao <41564015+kammanageswararao@users.noreply.github.com>
2019-05-25 23:32:30 +0530

    Add files via upload

---
+++ src/SonarQubeProject.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author knageswarar2
+ *
+ */
+public class SonarQubeProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first SonarQubeproject testing");
+
+	}
+
+}
